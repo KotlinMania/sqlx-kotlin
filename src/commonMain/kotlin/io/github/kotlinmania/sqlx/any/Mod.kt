7c50a8ce221c9c932a06fd7441ad097536e12615
Sydney Renee <sydney@solace.ofharmony.ai>
2026-08-24 01:57:43 -0700

    style(sqlx): fix ktlint issues in Lib.kt, any/Mod.kt, and SpecErrorTest.kt

--- src/commonMain/kotlin/io/github/kotlinmania/sqlx/any/Mod.kt
+++ src/commonMain/kotlin/io/github/kotlinmania/sqlx/any/Mod.kt
@@ -1,9 +1,7 @@
 // port-lint: source any/mod.rs
 package io.github.kotlinmania.sqlx.any
 
-import io.github.kotlinmania.sqlx.AnyConnection
 import io.github.kotlinmania.sqlx.AnyDriver
-import io.github.kotlinmania.sqlx.AnyPool
 import io.github.kotlinmania.sqlx.installDefaultDrivers as rootInstallDefaultDrivers
 import io.github.kotlinmania.sqlx.installDrivers as rootInstallDrivers
 
